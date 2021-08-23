0a0c6126f362c49c3d7851967ee21cd88afed43f
Marat <marat1989.ne@gmail.com>
2021-08-23 21:04:35 +0300

    Выделен вспомогательный класс GroupHelper для управления группами

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="new_group_1", header="new_group_header_1", footer="new_group_footer_1"))
+    app.group.create(Group(name="new_group_1", header="new_group_header_1", footer="new_group_footer_1"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
